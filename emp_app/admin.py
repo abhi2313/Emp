8ab0d50ac79ffc6139b85218e1861b1eb5b14edc
Abhishek Kumar Singh <abhishekchbsas@gmail.com>
2022-06-17 12:44:53 +0530

    first commit

---
+++ emp_app/admin.py
@@ -0,0 +1,8 @@
+import imp
+from django.contrib import admin
+from .models import Employee,Role,Department
+
+# Register your models here.
+admin.site.register(Role)
+admin.site.register(Department)
+admin.site.register(Employee)
